23c237f61c7e9bec69adfaddbe6dcfd3250caecf
Bartek1610 <111514028+Bartek1610@users.noreply.github.com>
2022-09-15 21:51:06 +0200

    Delete jotaro_coins directory

--- jotaro_coins/config.lua
+++
@@ -1,8 +0,0 @@
-Config = {}
-
-Config.Item = 'money' -- Jaki item ma być nadawany
-Config.Count = 500 -- Ilość nadawanego itemu
-Config.Time = 30 -- Czas co ile zostaje nadany item / W MINUTACH
-Config.Money = false -- Czy nagroda ma być w postaci pieniędzy (bank/gotówka) | NIE W PRZYPADKU HAJSU POD ITEM
-
-Config.Debug = false -- Debug mode, tworzy on komende która pozwala nam odpalić timer (timer odpala się tylko gdy gracz wejdzie na serwer)
